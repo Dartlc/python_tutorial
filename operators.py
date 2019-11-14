7afb1df3dc6a1740a0461efb61b052e51cc6d134
Dagwood <sudhagarnarayanan@gmail.com>
2019-11-14 12:04:14 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,136 @@
+"""
+    python has 7 types of operators
+
+    1. arithmetic operators
+    2. assignment operators
+    3. comparison operators
+    4. logical operators
+    5. identity operators
+    6. membership operators
+    7. bitwise operators
+
+"""
+
+"""
+    Arithmetic operators
+        addition - '+'
+        subtraction - '-'
+        Division - '/'
+        multiplication - '*'
+        modulus - '%'
+        exponential - '**'
+        floor division - '//' 
+        
+"""
+a = 15  # Variable initiation
+b = 3
+
+"""
+    addition operators
+"""
+c = a + b
+print(f"The addition of two number is {c}")  # Addition result for two number
+
+"""
+    subtraction operators
+"""
+c = a - b
+print(f"The subtraction of two number is {c}")  # Subtraction result for two numbers
+
+"""
+    multiplication operators
+"""
+c = a * b
+print(f"The multiplication of two number is {c}")  # Multiplication result for two numbers
+
+"""
+    Division operators
+"""
+c = a / b
+print(f"The Division of two number is {c}")  # Division result for two numbers
+
+"""
+    modulus operator
+"""
+c = a % b
+print(f"The modulus of two number is {c}")  # The result will be a remainder of divided operation
+
+"""
+    exponential operator
+"""
+c = a ** b
+print(
+    f"The exponential of two number is {c}")  # The result will be calculate the exponential value with the base set to e.
+
+"""
+    floor division
+"""
+c = a // b
+print(f"The addition of two number is {c}")  # Returns the integral part of the quotient.
+
+"""
+    Assignment operators
+"""
+a = 15
+
+a += 3  # a = a + 3
+print(f"The result will be incremented by 3 {a}")  # The result will be incremented by 3
+
+a -= 3  # a = a - 3
+print(f"The result will be decremented by 3 {a}")  # The result will be decremented by 3
+
+a *= 3  # a = a * 3
+print(f"The result will be multiplied by 3 {a}")  # The result will be multiplied by 3
+
+a /= 3  # a = a / 3
+print(f"The result will be divided by 3 {a}")  # The result will be divided by 3
+
+a %= 3  # a = a % 3
+print(f"The remainder will be {a}")  # The result will be a remainder of divided operation
+
+a **= 3  # a = a ** 3
+print(
+    f"The exponential of two number is {a}")  # The result will be calculate the exponential value with the base set to e.
+
+a //= 3  # a = a // 3
+print(f"The addition of two number is {a}")  # Returns the integral part of the quotient.
+
+"""
+    comparison operators
+"""
+
+"""
+    Equal
+"""
+c = a == b
+print(f"The comparison between a and b is {c}")  # The Result will be in boolean format
+
+"""
+    Not equal
+"""
+c = a != b
+print(f"The comparison between a and b is {c}")  # The Result will be in boolean format
+
+"""
+    Less than
+"""
+c = a < b
+print(f"The comparison between a and b is {c}")  # The Result will be in boolean format
+
+"""
+    Greater than
+"""
+c = a > b
+print(f"The comparison between a and b is {c}")  # The Result will be in boolean format
+
+"""
+    Less than or equal
+"""
+c = a <= b
+print(f"The comparison between a and b is {c}")  # The Result will be in boolean format
+
+"""
+    Greater than or equal
+"""
+c = a >= b
+print(f"The comparison between a and b is {c}")  # The Result will be in boolean format
