db02fe2d0a12db2da4fc9818b47138018b65b32f
Dagwood <sudhagarnarayanan@gmail.com>
2019-12-13 17:05:00 +0530

    file handling

--- Examples/pickling_unpickling.py
+++ Examples/pickling_unpickling.py
@@ -2,11 +2,11 @@ import pickle
 
 a = {1: "A", 2: "B", 3: "c"}
 
-pickle_on = open("EMP.pickle", "wb")
+pickle_on = open("EMP.txt", "wb")
 pickle.dump(a, pickle_on)
 pickle_on.close()
 
-pickle_off = open("EMP.pickle", "rb")
+pickle_off = open("EMP.txt", "rb")
 a = pickle.load(pickle_off)
 print(a)
 
--- Examples/split_func.py
+++ Examples/split_func.py
@@ -1,6 +1,11 @@
 input_value = "name:sudhagar age:29 email:sudhagar@gmail.com"
 
 x = input_value.split(" ")
+dict_1 = {}
 for i in x:
     output = i.split(":")
-    print(output)
+    dict_1[output[0]] = output[1]
+print(dict_1)
+
+result = dict(i.split(":") for i in input_value.split(" "))
+print(result)
